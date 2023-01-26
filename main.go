cd0b261cbb57793e10cc3f17f8d546b4f4a93cd0
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-01-26 14:12:02 +0530

    go goroutine added

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
+	"time"
 )
 
 type UserData struct {
@@ -11,6 +13,8 @@ type UserData struct {
 	Email     string
 	Tickets   uint
 }
+
+var wg = sync.WaitGroup{}
 
 func main() {
 	conferenceName := "Go Conference"
@@ -22,6 +26,7 @@ func main() {
 	var email string
 	var userTickets uint
 	bookTickets(firstName, lastName, email, userTickets, remainingTickets)
+
 }
 
 // It takes in a first name, last name, email, user tickets and remaining tickets as arguments, and
@@ -29,37 +34,36 @@ func main() {
 func bookTickets(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) {
 	var userData UserData
 	var bookings = make([]UserData, 0)
-	for {
+	fmt.Println("Please enter your first name:")
+	fmt.Scanln(&firstName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scanln(&lastName)
+	fmt.Println("Please enter your email:")
+	fmt.Scanln(&email)
+	fmt.Println("Please enter the number of tickets you want to buy")
+	fmt.Scanln(&userTickets)
+	userData.FirstName = firstName
+	userData.LastName = lastName
+	userData.Email = email
+	userData.Tickets = userTickets
+	fmt.Printf("userData: %v \n", userData)
+	fmt.Printf("Thank you, %v %v, you have bought %v tickets! you will recieve the confirmation mail at %v \nSee you at the conference. \n", firstName, lastName, userTickets, email)
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTickets := userTickets > 0 && userTickets <= remainingTickets
+	if isValidEmail && isValidName && isValidTickets {
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, userData)
 
-		fmt.Println("Please enter your first name:")
-		fmt.Scanln(&firstName)
-		fmt.Println("Please enter your last name:")
-		fmt.Scanln(&lastName)
-		fmt.Println("Please enter your email:")
-		fmt.Scanln(&email)
-		fmt.Println("Please enter the number of tickets you want to buy")
-		fmt.Scanln(&userTickets)
-		userData.FirstName = firstName
-		userData.LastName = lastName
-		userData.Email = email
-		userData.Tickets = userTickets
-		fmt.Printf("userData: %v \n", userData)
-		fmt.Printf("Thank you, %v %v, you have bought %v tickets! you will recieve the confirmation mail at %v \nSee you at the conference. \n", firstName, lastName, userTickets, email)
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
-		if isValidEmail && isValidName && isValidTickets {
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, userData)
+		firstName := printFirstNames(bookings)
+		fmt.Printf("firstName: %v \n", firstName)
 
-			firstName := printFirstNames(bookings)
-			fmt.Printf("firstName: %v \n", firstName)
-
-		} else {
-			fmt.Println("Your input value are invalid try again")
-		}
-
+	} else {
+		fmt.Println("Your input value are invalid try again")
 	}
+	wg.Add(1)
+	go sendTicketViaEmailHandler(firstName, lastName, email, userTickets)
+	wg.Wait()
 }
 
 // It takes in three parameters, a string, an int, and a uint, and prints out a message to the user
@@ -76,4 +80,12 @@ func printFirstNames(bookings []UserData) []string {
 		firstNames = append(firstNames, booking.FirstName)
 	}
 	return firstNames
+}
+func sendTicketViaEmailHandler(firstName string, lastName string, email string, userTickets uint) {
+	time.Sleep(10 * time.Second)
+	var tickets = fmt.Sprintf("%v ticket for %v %v", userTickets, firstName, lastName)
+	fmt.Println("******************************************")
+	fmt.Printf("Sending ticket: \n %v \n to email address %v \n", tickets, email)
+	fmt.Println("******************************************")
+	wg.Done()
 }
